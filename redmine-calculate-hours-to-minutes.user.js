c75269f32f8bfed1d16eb94250c4cdd57585a5e5
rgbfreak <rgbfreak@users.noreply.github.com>
2022-05-25 11:02:10 +0200

    Bugfix
    Bei Eltern Tickets funktioniert die Berechnung nun auch

--- redmine-calculate-hours-to-minutes.user.js
+++ redmine-calculate-hours-to-minutes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Redmine calculate Hours to Minutes
 // @icon        https://kunden.redmine.imos.net/favicon.ico
-// @version     1.0.0
+// @version     1.0.1
 // @downloadURL https://raw.githubusercontent.com/imosnet/userscripts/master/redmine-calculate-hours-to-minutes.user.js
 // @match       *://kunden.redmine.imos.net/*
 // @match       *://intern.redmine.imos.net/*
@@ -11,9 +11,17 @@
 
 jQuery.noConflict(true)(function($) {
     if($('.estimated-hours .value').length) {
-        $('.estimated-hours .value').append('<span> = ' + Math.round($('.estimated-hours .value').html().replace(' h', '') * 3600 / 60) + ' min</span>');
+        let hours = $('.estimated-hours .value').html();
+        hours = hours.replace(' (Gesamtzahl: ', '')
+        hours = hours.replace(')', '')
+        hours = parseFloat(hours.replace(' h', ''))
+
+        $('.estimated-hours .value').append('<span> = ' + Math.round(hours * 3600 / 60) + ' min</span>');
     }
     if($('.spent-time .value a').length) {
-        $('.spent-time .value a').append('<span> = ' + Math.round($('.spent-time .value a').html().replace(' h', '') * 3600 / 60) + ' min</span>');
+        let hours = $('.spent-time .value a').html();
+        hours = parseFloat(hours.replace(' h', ''))
+
+        $('.spent-time .value a').append('<span> = ' + Math.round(hours * 3600 / 60) + ' min</span>');
     }
 });
