ca66153f131a984354588b51b423022b80d320e9
rgbfreak <rgbfreak@users.noreply.github.com>
2022-07-20 18:56:36 +0200

    redmine-omments-always-private.js erstellt

---
+++ redmine-omments-always-private.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Redmine comments always private
+// @icon        https://kunden.redmine.imos.net/favicon.ico
+// @version     1.0
+// @downloadURL https://raw.githubusercontent.com/imosnet/userscripts/master/redmine-title-copy-for-commit.user.js
+// @include     http*://kunden.redmine.imos.net/*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
+// @grant       none
+// ==/UserScript==
+
+jQuery.noConflict(true)(function($) {
+    $('#issue_private_notes').prop('checked', true);
+});
